342c32488caa10b9540bdf59c80275b812f1b8d2
Ann Clawson <annmasonclawson@gmail.com>
2023-12-21 10:31:57 -0800

    creates user routes and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_admin, except: [:create, :update, :show, :delete]
+  before_action :authenticate_user, except: [:create]
+
+  # def initialize(user)
+  #   can :manage, :all if user.admin == true
+  # end
+
   def create
-    user = User.new(
+    @user = User.new(
       last_name: params[:last_name],
       first_name: params[:first_name],
       email: params[:email],
@@ -9,10 +16,9 @@ class UsersController < ApplicationController
       user_name: params[:user_name],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
-      admin: params[:admin] || false,
-
+      admin: params[:admin],
     )
-    if user.save
+    if @user.save
       render json: { message: "User created successfully" }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
@@ -20,14 +26,37 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: current_user.id)
+    @user.update(
+      last_name: params[:last_name] || @user.last_name,
+      first_name: params[:first_name] || @user.first_name,
+      email: params[:email] || @user.email,
+      address: params[:address] || @user.address,
+      phone_number: params[:phone_number] || @user.phone_number,
+      user_name: params[:user_name] || @user.user_name,
+      password: params[:password] || @user.password,
+      password_confirmation: params[:password_confirmation] || @user.password_confirmation,
+    )
+    if @user.valid?
+      render :show
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @user = User.find_by(id: current_user.id)
+    @user.destroy
+    render json: { message: "user successfully deleted" }
   end
 
   def show
+    @user = User.find_by(id: current_user.id)
+    render :show
   end
 
   def index
+    @users = User.all
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+  get "/users/:id" => "users#show"
+  get "/users" => "users#index"
 
   post "/sessions" => "sessions#create"
 end
