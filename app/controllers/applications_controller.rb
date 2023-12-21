f3dc4caf7319955b85c42edce37d114edd06ab48
Ann Clawson <annmasonclawson@gmail.com>
2023-12-21 13:14:58 -0800

    creates applications routes, controllers, ansd cations

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,54 +1,53 @@
 class ApplicationsController < ApplicationController
+  before_action :authenticate_user
+
   def create
     @application = Application.new(
-      user_id: params[:current_user.id],
-      tattooer_id: params[:tattooer.id],
+      user_id: current_user.id,
+      tattooer_id: params[:tattooer_id],
       amount: params[:amount],
       number_of_months: params[:number_of_months],
       date_of_appt: params[:date_of_appt],
       description: params[:description],
       approved: params[:approved],
-
     )
     if @application.save
       render json: { message: "Application created successfully" }, status: :created
     else
-      render json: { errors: application.errors.full_messages }, status: :bad_request
+      render json: { errors: @application.errors.full_messages }, status: :bad_request
     end
   end
 
   def update
-    @user = User.find_by(id: current_user.id)
-    @user.update(
-      last_name: params[:last_name] || @user.last_name,
-      first_name: params[:first_name] || @user.first_name,
-      email: params[:email] || @user.email,
-      address: params[:address] || @user.address,
-      phone_number: params[:phone_number] || @user.phone_number,
-      user_name: params[:user_name] || @user.user_name,
-      password: params[:password] || @user.password,
-      password_confirmation: params[:password_confirmation] || @user.password_confirmation,
+    @application = Application.find_by(id: params[:id])
+    @application.update(
+      tattooer_id: params[:tattooer_id] || @application.tattooer_id,
+      amount: params[:amount] || @application.amount,
+      number_of_months: params[:number_of_months] || @application.number_of_months,
+      date_of_appt: params[:date_of_appt] || @application.date_of_appt,
+      description: params[:description] || @application.description,
+      approved: params[:approved] || @application.approved,
     )
-    if @user.valid?
+    if @application.valid?
       render :show
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @application.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user = User.find_by(id: current_user.id)
-    @user.destroy
-    render json: { message: "user successfully deleted" }
+    @application = Application.find_by(id: params[:id])
+    @application.destroy
+    render json: { message: "Application successfully deleted" }
   end
 
   def show
-    @user = User.find_by(id: current_user.id)
+    @application = Application.find_by(id: params[:id])
     render :show
   end
 
   def index
-    @users = User.all
+    @applications = Application.all
     render :index
   end
 end
