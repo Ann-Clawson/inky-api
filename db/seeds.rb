06d8302de50d06bd1261e5306b246b303820ce63
Ann Clawson <annmasonclawson@gmail.com>
2024-02-20 14:19:19 -0800

    seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# User.create!([
-#   { last_name: "Clawson", first_name: "Eno", email: "Eno@eno.com", user_name: "Enopaneno", password: "password", address: "home", phone_number: "555-1234" },
-# ])
+User.create!([
+  { last_name: "Clawson", first_name: "Eno", email: "Eno@eno.com", password: "password", address: "home", phone_number: "555-1234" },
+])
 
-# Tattooer.create!([
-#   { last_name: "Davis", first_name: "Jen", email: "jen@jen.com", user_name: "JenDavis", password: "password", address: "123 Main St.", phone_number: "555-4321", tattoo_shop: "Outer Limits" },
-# ])
+Tattooer.create!([
+  { last_name: "Davis", first_name: "Jen", email: "jen@jen.com", password: "password", phone_number: "555-4321", tattoo_shop: "Outer Limits" },
+])
 
-# Application.create!([
-#   { user_id: 1, tattooer_id: 1, amount: 100, number_of_months: 5, date_of_appt: "01/01/24", approved: true, description: "butterfly" },
-# ])
+Application.create!([
+  { user_id: 1, tattooer_id: 1, amount: 100, number_of_months: 5, date_of_appt: "01/01/24", approved: true, description: "butterfly" },
+])
