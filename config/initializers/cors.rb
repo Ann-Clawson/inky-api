b79372001397b104a5fe157ed46669b96620d308
Ann Clawson <annmasonclawson@gmail.com>
2024-02-21 15:16:20 -0800

    reconfigs rack cors third time

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*", "localhost:5173"
+    origins "/localhost:\d+/", "*"
 
     resource "*",
       headers: :any,
