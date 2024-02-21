71982e18ac54b12b8f0726110943f23b62cc2c14
Ann Clawson <annmasonclawson@gmail.com>
2024-02-21 15:14:00 -0800

    reconfigs rack cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://inky-frontend.onrender.com", "localhost:5173"
+    origins "*", "localhost:5173"
 
     resource "*",
       headers: :any,
